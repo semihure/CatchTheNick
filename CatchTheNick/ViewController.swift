77839bb17e54a6fb27b593a2dc07af65aa29852f
hellosemih <semihure@gmail.com>
2019-09-04 16:45:25 +0300

    Initial Commit

---
+++ CatchTheNick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTheNick
+//
+//  Created by smh on 4.09.2019.
+//  Copyright © 2019 smh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
